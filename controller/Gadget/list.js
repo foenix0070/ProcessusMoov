0a8d08e4d1e5dcc6d7153942bb9f94e96b1c29fb
dao coul <dcoulibaly634@gmail.com>
2023-07-25 13:13:30 +0000

    GADGET.V0.1

---
+++ controller/Gadget/list.js
@@ -0,0 +1,102 @@
+var ListConge = ListConge || {};
+var clientContext;
+ListConge.clientContext;
+
+ListConge.InitializePage = function () {
+  ListConge.clientContext = SP.ClientContext.get_current();
+  clientContext =  SP.ClientContext.get_current();
+
+
+  let T =  appHelper.GetQueryStringFromAjaxQuery('t');
+  let x = document.getElementById('h2Titre');
+  switch(T){
+    case 'E' :  ListConge.ListConge('ENCOURS');
+                x.innerHTML = " Listes des demandes de congés en cours";
+    break;
+    case 'V' :  ListConge.ListConge('VALIDEE');
+    x.innerHTML = " Listes des demandes de congés validées";
+    break;
+    case 'R' :  ListConge.ListConge('REJETEE');
+    x.innerHTML = " Listes des demandes de congés rejétées";
+    break;
+    default : ListConge.ListConge('ENCOURS');
+    x.innerHTML = " Listes des demandes de congés en cours";
+    break;
+  }
+
+
+};
+
+
+
+
+
+ListConge.ListConge = function (T) {
+  let oList = ListConge.clientContext
+    .get_web()
+    .get_lists()
+    .getByTitle(appHelper.ListName.Conge);
+  let camlQuery = new SP.CamlQuery();
+  camlQuery.set_viewXml(
+    "<View><Query><Where>" +
+      '<And>'+
+      '<Eq><FieldRef ID="Demandeur" /><Value Type="Integer"><UserID/></Value></Eq>' +
+      '<Eq><FieldRef ID="Statut" /><Value Type="Choice">'+ T +'</Value></Eq>' +
+      '</And>'+
+      "</Where></Query></View>"
+  );
+  let collListItem = oList.getItems(camlQuery);
+  ListConge.clientContext.load(collListItem);
+  ListConge.clientContext.executeQueryAsync(function (sender, args) {
+    if (collListItem.get_count() > 0) {
+      var listItemEnumerator = collListItem.getEnumerator();
+      let view = {};
+      view.conges = [];
+      while (listItemEnumerator.moveNext()) {
+        var oListItem = listItemEnumerator.get_current();
+        view.conges.push({
+          id: oListItem.get_item("ID"),
+          title: oListItem.get_item("Title"),
+          startdate: new Date( oListItem.get_item("DateDepart")).toLocaleDateString(),
+          nbre: oListItem.get_item("NombreJours"),
+          status: oListItem.get_item("StatutLibelle"),
+          classe: appHelper.Status.GetClass(oListItem.get_item("Statut")),
+        });
+      }
+
+      appHelper.renderTemplate("tmpl_table_conge", "DivCongeTableShow", view);
+
+   //   appHelper.listenNavigationLink ('linkMainNavigation');
+      const linkClick = document.getElementsByClassName('click');
+      for (var i = 0; i < linkClick.length; i++) {
+        linkClick[i].addEventListener("click", function () {
+          let url = this.getAttribute("data-url");
+          sessionStorage.setItem("ajax_url", url);
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: 'html',
+                success: function (data) {
+                    $('#reponseAjax').html(data);
+                },
+                error: function () {
+                    $('#reponseAjax').html('Erreur lors du chargement des données.');
+                }
+            });
+
+          return false;
+        });
+      }
+
+    }
+  }, appSpHelper.writeError);
+};
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   ExecuteOrDelayUntilScriptLoaded(function(){
+  SP.SOD.executeFunc('sp.js', 'SP.ClientContext', ListConge.InitializePage);
+  //   }, "SP.ClientContext");
+  // });
